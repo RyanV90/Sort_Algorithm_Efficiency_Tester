8743816052657439ebc0432b0f11b13f38ff32e4
Ryan Veitenheimer <rmveit@gmail.com>
2021-01-08 18:24:14 -0500

    Created constructor for insertion sort

--- Sort_Algorithm_Efficiency_Tester/InsertionSort.h
+++ Sort_Algorithm_Efficiency_Tester/InsertionSort.h
@@ -1,5 +1,26 @@
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class InsertionSort
 {
-};
+public:
 
+	template<typename T> InsertionSort(vector<T>& vector) {
+
+		for (int i = 1; i < vector.size(); i++)
+		{
+			int j = i;
+			while (j > 0 && (vector[j] < vector[j - 1])) //insert the ith element into sorted list 
+			{
+				T temp = vector[j];
+				vector[j] = vector[j - 1];
+				vector[j - 1] = temp;
+				j--; // move index j back
+			}
+		}
+	}
+
+};
